17f820231b395e878388281b21767e5691b2759e
Парасюк Виктория <107310178+parrusVika@users.noreply.github.com>
2022-11-12 23:27:24 +0300

    summry

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+
+startBtn.addEventListener('click', onColorBody);
+stopBtn.addEventListener('click', onStopColorBody)
+
+let intervalId = null
+
+function onColorBody() {
+    startBtn.setAttribute('disabled', true);
+    intervalId = setInterval(() => {
+
+        document.body.style.backgroundColor = getRandomHexColor();
+
+    }, 1000)
+
+};
+
+function onStopColorBody() {
+    clearInterval(intervalId);
+    startBtn.removeAttribute('disabled');
+
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+
+
 
