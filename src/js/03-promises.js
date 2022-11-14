f90a136405ece79ea2e9311936c7773a4eac4eee
Парасюк Виктория <107310178+parrusVika@users.noreply.github.com>
2022-11-15 00:12:59 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,26 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', onBtnSub);
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  setTimeout(() => {
+    if (shouldResolve) {
+      resolve({ position, delay });
+    } else {
+      reject({ position, delay });
+    }
+  }, delay);
+
+};
+
+function onBtnSub(e) {
+  e.preventDefult()
 }
+
+const FirstDelay = Number(e.currentTargete.elements.delay.value);
+const DelayStep = Number(e.currentTarget.elements.step.value);
+const Amount = Number(e.currentTarget.elements.amount.value);
+
